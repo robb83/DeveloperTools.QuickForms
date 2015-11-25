1faef683a73b3c833f07b3d15b66559d96954f96
Róbert Oláh <robb83@gmail.com>
2015-11-25 22:57:53 +0100

    GridForm - proof of concept, rewrite soon

--- DeveloperTools.QuickForms.Sample/MainForm.Designer.cs
+++ DeveloperTools.QuickForms.Sample/MainForm.Designer.cs
@@ -32,6 +32,8 @@
             this.buttonClose = new System.Windows.Forms.Button();
             this.buttonCreateNewProject = new System.Windows.Forms.Button();
             this.buttonCreateTodo = new System.Windows.Forms.Button();
+            this.buttonShowTodoList = new System.Windows.Forms.Button();
+            this.buttonShowAdvancedTodoList = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // buttonCreateSettings
@@ -46,7 +48,7 @@
             // 
             // buttonClose
             // 
-            this.buttonClose.Location = new System.Drawing.Point(197, 227);
+            this.buttonClose.Location = new System.Drawing.Point(197, 375);
             this.buttonClose.Name = "buttonClose";
             this.buttonClose.Size = new System.Drawing.Size(75, 23);
             this.buttonClose.TabIndex = 1;
@@ -74,11 +76,33 @@
             this.buttonCreateTodo.UseVisualStyleBackColor = true;
             this.buttonCreateTodo.Click += new System.EventHandler(this.buttonCreateTodo_Click);
             // 
+            // buttonShowTodoList
+            // 
+            this.buttonShowTodoList.Location = new System.Drawing.Point(12, 219);
+            this.buttonShowTodoList.Name = "buttonShowTodoList";
+            this.buttonShowTodoList.Size = new System.Drawing.Size(260, 63);
+            this.buttonShowTodoList.TabIndex = 4;
+            this.buttonShowTodoList.Text = "Show TODO List";
+            this.buttonShowTodoList.UseVisualStyleBackColor = true;
+            this.buttonShowTodoList.Click += new System.EventHandler(this.buttonShowTodoList_Click);
+            // 
+            // buttonShowAdvancedTodoList
+            // 
+            this.buttonShowAdvancedTodoList.Location = new System.Drawing.Point(12, 288);
+            this.buttonShowAdvancedTodoList.Name = "buttonShowAdvancedTodoList";
+            this.buttonShowAdvancedTodoList.Size = new System.Drawing.Size(260, 63);
+            this.buttonShowAdvancedTodoList.TabIndex = 5;
+            this.buttonShowAdvancedTodoList.Text = "Show Advanced TODO List";
+            this.buttonShowAdvancedTodoList.UseVisualStyleBackColor = true;
+            this.buttonShowAdvancedTodoList.Click += new System.EventHandler(this.buttonShowAdvancedTodoList_Click);
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(284, 414);
+            this.Controls.Add(this.buttonShowAdvancedTodoList);
+            this.Controls.Add(this.buttonShowTodoList);
             this.Controls.Add(this.buttonCreateTodo);
             this.Controls.Add(this.buttonCreateNewProject);
             this.Controls.Add(this.buttonClose);
@@ -95,6 +119,8 @@
         private System.Windows.Forms.Button buttonClose;
         private System.Windows.Forms.Button buttonCreateNewProject;
         private System.Windows.Forms.Button buttonCreateTodo;
+        private System.Windows.Forms.Button buttonShowTodoList;
+        private System.Windows.Forms.Button buttonShowAdvancedTodoList;
     }
 }
 
--- DeveloperTools.QuickForms.Sample/MainForm.cs
+++ DeveloperTools.QuickForms.Sample/MainForm.cs
@@ -66,5 +66,131 @@ namespace DeveloperTools.QuickForms.Sample
                 // canceled, nothing to do
             }
         }
+
+        private void buttonShowTodoList_Click(object sender, EventArgs e)
+        {
+            var todoModel1 = new TodoModel();
+            todoModel1.Description = "Hello World #1";
+            todoModel1.EventDate = DateTime.Today.AddDays(7);
+
+            var todoModel2 = new TodoModel();
+            todoModel2.Description = "Hello World #2";
+            todoModel2.EventDate = DateTime.Today.AddDays(7);
+
+            List<TodoModel> datasource = new List<TodoModel>();
+            datasource.Add(todoModel1);
+            datasource.Add(todoModel2);
+
+            GridForm<TodoModel>.ShowDialog(datasource);
+        }
+
+        private void buttonShowAdvancedTodoList_Click(object sender, EventArgs e)
+        {
+            var todoModel1 = new TodoModel();
+            todoModel1.Description = "Hello World #1";
+            todoModel1.EventDate = DateTime.Today.AddDays(7);
+
+            var todoModel2 = new TodoModel();
+            todoModel2.Description = "Hello World #2";
+            todoModel2.EventDate = DateTime.Today.AddDays(7);
+
+            List<TodoModel> datasource = new List<TodoModel>();
+            datasource.Add(todoModel1);
+            datasource.Add(todoModel2);
+
+            GridForm<TodoModel>.ShowDialog(new AdvancedGridHandler(datasource));
+        }
+
+        class AdvancedGridHandler : IGridFormHandler<TodoModel>
+        {
+            List<TodoModel> entities;
+            List<CustomGridAction> actions;
+
+            public AdvancedGridHandler(List<TodoModel> entities)
+            {
+                this.entities = entities;
+                this.actions = new List<CustomGridAction>
+                {
+                    new CustomGridAction
+                    {
+                        Name = "Export",
+                        SelectionType = CustomGridActionSelectionType.None,
+                        Text = "Export"
+                    },
+                    new CustomGridAction
+                    {
+                        Name = "DeleteSelectedRows",
+                        SelectionType = CustomGridActionSelectionType.Multiple,
+                        Text = "Delete Selected Rows"
+                    },
+                    new CustomGridAction
+                    {
+                        Name = "CreateNew",
+                        SelectionType = CustomGridActionSelectionType.None,
+                        Text = "CreateNew"
+                    }
+                };
+            }
+
+            public void BeginEdit(TodoModel model)
+            {
+                
+            }
+
+            public void CancelEdit(TodoModel model)
+            {
+                
+            }
+
+            public void EndEdit(TodoModel model)
+            {
+
+            }
+
+            public bool CanEdit(TodoModel model)
+            {
+                return true;
+            }
+
+            public List<CustomGridAction> GetCustomActions()
+            {
+                return this.actions;
+            }
+
+            public List<TodoModel> GetData()
+            {
+                return this.entities;
+            }
+
+            public void HandleCustomAction(CustomGridAction action, IGrid<TodoModel> grid)
+            {
+                if (action.Name == this.actions[1].Name)
+                {
+                    List<TodoModel> selectedItems = grid.GetSelectedRows();
+                    foreach(TodoModel model in selectedItems)
+                    {
+                        this.entities.Remove(model);
+                    }
+
+                    grid.SetDataSource(this.entities);
+                }
+                else if (action.Name == this.actions[2].Name)
+                {
+                    var todoModel = new TodoModel();
+                    todoModel.Description = "Hello World #" + this.entities.Count;
+                    todoModel.EventDate = DateTime.Today.AddDays(7);
+
+                    if (EditorForm<TodoModel>.ShowDialog(todoModel))
+                    {
+                        this.entities.Add(todoModel);
+                        grid.SetDataSource(this.entities);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show(action.Name);
+                }
+            }
+        }
     }
 }
